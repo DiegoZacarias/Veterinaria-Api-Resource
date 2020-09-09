d411f14b11e8f6370872bb61f08174535446cdb7
DiegoZacarias <diegozaca97@gmail.com>
2020-09-09 12:41:17 -0400

    CitaTest feature - Ya se pueden crear las citas

---
+++ app/Http/Controllers/API/CitaController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Cita;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CitaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // dd($request->all());
+
+        $cita = Cita::create($request->all());
+
+        return $cita;
+
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return false;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        return false;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- tests/Feature/CitaTest.php
+++ tests/Feature/CitaTest.php
@@ -2,21 +2,40 @@
 
 namespace Tests\Feature;
 
+use App\Cita;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class CitaTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function testExample()
-    {
-        $response = $this->get('/');
+  use RefreshDatabase, WithFaker;
+    
 
-        $response->assertStatus(200);
+  /** @test */
+  public function se_puede_agregar_citas()
+  {
+      $fields = [
+          'nombre_mascota' => 'Ketti',
+          'nombre_dueno' => 'Diego',
+          'fecha' => $this->faker->date($format = 'Y-m-d', $max = 'now'),
+          'hora' => $this->faker->time($format = 'H:i', $max = 'now'),
+          'sintomas' => 'Ser genial'
+      ];
+
+      $this->withoutExceptionHandling();
+      $this->post(route('citas.store'),$fields); //store
+
+      $this->assertDatabaseHas('citas',$fields);
+  }
+
+    /** @test */
+    public function se_puede_listas_todas_las_citas()
+    {
+        $citas = factory(Cita::class)->create();
+
+        $this->withoutExceptionHandling();
+        $this->get(route('citas.index'))->assertStatus(200);
+
     }
 }
